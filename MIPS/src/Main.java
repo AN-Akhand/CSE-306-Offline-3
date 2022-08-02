a352846d4b793d3fb0da07f41d9d63d164df3fdb
ezmata-101 <1805090@ugrad.cse.buet.ac.bd>
2022-08-02 14:31:52 +0600

    MIPS to bin

---
+++ MIPS/src/Main.java
@@ -0,0 +1,61 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static boolean isR(int i){return i == 1 || i == 2 || i == 3 || i == 7 || i == 9;}
+
+    static boolean isI(int i) {return i == 0 || i == 4 || i == 11 || i == 12 || i == 13 || i == 14;}
+    static String getBin(int n){
+        String s = Integer.toBinaryString(n);
+        int len = s.length();
+        if(len == 1) s = "000"+s;
+        if(len == 2) s = "00"+s;
+        if(len == 3) s = "0"+s;
+        return s;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> opcodes = List.of("beq", "add", "nor", "or", "addi", "sll", "sw", "sub", "j", "and", "srl", "ori", "bneq", "subi", "andi","lw");
+        List<String> registers = List.of("$zero", "$t0", "$t1", "$t2", "$t3", "$t4");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("text.txt")));
+        String instruction;
+        String ops;
+        String[] operands;
+        StringBuilder binIns;
+        int opcode;
+
+        while(true){
+            binIns = new StringBuilder();
+            instruction = reader.readLine();
+            if(instruction == null) break;
+            System.out.println(instruction);
+            ops = instruction.split(" ")[0];
+            instruction = instruction.replace(ops, "").replace(" ", "");
+            opcode = opcodes.indexOf(ops);
+            if(isR(opcode)){
+                binIns.append(Integer.toHexString(opcode));
+                operands = instruction.split(",");
+                binIns.append(Integer.toHexString(registers.indexOf(operands[1])));
+                binIns.append(Integer.toHexString(registers.indexOf(operands[2])));
+                binIns.append(Integer.toHexString(registers.indexOf(operands[0])));
+                System.out.println(binIns);
+            }
+            if(isI(opcode)){
+                binIns.append(Integer.toHexString(opcode));
+                operands = instruction.split(",");
+                binIns.append(Integer.toHexString(registers.indexOf(operands[1])));
+                binIns.append(Integer.toHexString(registers.indexOf(operands[0])));
+                binIns.append(Integer.toHexString(Integer.parseInt(operands[2])));
+                System.out.println(binIns);
+            }
+            if(opcode == 6 || opcode == 15){
+
+            }
+        }
+    }
+}
